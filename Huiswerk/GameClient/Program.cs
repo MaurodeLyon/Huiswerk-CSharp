d4593d02b416dcc775ecc2f5c1684a7e18fb0235
Mauro de Lyon <maurodelyon@gmail.com>
2015-10-16 11:58:04 +0200

    start mini project

---
+++ Huiswerk/GameClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using CommunicationLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(ClientThread).Start();
+        }
+
+        private static void ClientThread()
+        {
+            TcpClient client = new TcpClient("145.48.226.96", 1330);
+            bool done = false;
+            Console.WriteLine("Type 'bye' to end connection");
+            while (!done)
+            {
+                Console.Write("Enter a message to send to server: ");
+                string message = Console.ReadLine();
+
+                Communication.WriteTextMessage(client, message);
+
+                string response = Communication.ReadTextMessage(client);
+                Console.WriteLine("Response: " + response);
+                done = response.Equals("BYE");
+            }
+        }
+    }
+}
---
+++ Huiswerk/GameServer/Program.cs
@@ -0,0 +1,55 @@
+﻿using CommunicationLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress localhost;
+            bool IsIPOk = IPAddress.TryParse("145.48.226.96", out localhost);
+            TcpListener listener = new System.Net.Sockets.TcpListener(localhost, 1330);
+
+            // Starts listening for incoming connection requests in the listener-thread:
+            listener.Start();
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for connection");
+
+                //AcceptTcpClient waits for a connection from the client:
+                TcpClient client = listener.AcceptTcpClient();
+
+                // handle this client in a new worker-thread 
+                // and continue accepting new clients:
+                Thread thread = new Thread(HandleClientThread);
+                thread.Start(client);
+            }
+        }
+        private static void HandleClientThread(object obj)
+        {
+            TcpClient client = obj as TcpClient;
+            bool done = false;
+            while (!done)
+            {
+                string received = Communication.ReadTextMessage(client);
+                string[] split = received.Split('|');
+                Console.WriteLine("{0}", split[0]);
+
+                done = split[0].Equals("bye");
+                if (done) Communication.WriteTextMessage(client, "BYE");
+                else Communication.WriteTextMessage(client, "OK");
+            }
+            client.Close();
+            Console.WriteLine("Connection closed");
+        }
+    }
+}
